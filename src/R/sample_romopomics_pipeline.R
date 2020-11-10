156af85fcaaad2b9e641ec56fd0c2911dc89cca6
clugstonA <anc174@pitt.edu>
2020-11-10 11:04:11 -0500

    Scrap script for gathering VCF metadata, may be useful but not right now.

--- src/R/sample_romopomics_pipeline.R
+++ src/R/sample_romopomics_pipeline.R
@@ -26,35 +26,3 @@ db_inputs   <- combineInputTables(input_table_list = omop_inputs)
 omop_db     <- buildSQLDBR(omop_tables = db_inputs, sql_db_file=file.path("../../data/sample.sqlite"))
 DBI::dbDisconnect(omop_db)
 
-
-#Reading VCF files.
-vcf_dir     <- "/projects/andrew/GrOMOP/test_data"
-vcf_tbl     <- tibble(file=dir(vcf_dir,full.names = TRUE)) %>%
-  filter(grepl("\\.vcf$|\\.vcf\\.gz$",file)) %>%
-  mutate(name=gsub(".vcf|.vcf.gz","",basename(file))) %>%
-  rowwise() %>%
-  mutate(size=utils:::format.object_size(file.size(file),unit="auto"),
-         gzipped = grepl(".gz$",file),
-         hdr_lines=system(intern=TRUE,paste(ifelse(gzipped,"zcat","cat"),file,"| grep ^# | wc -l")),
-         obs_lines=system(intern=TRUE,paste(ifelse(gzipped,"zcat","cat"),file,"| grep -v ^# | wc -l")))
-library(vcfR)
-#Read VCF:
-vcf <- read.vcfR(vcf_tbl$file[2])
-head(vcf)
-
-#Get metadata:
-queryMETA(vcf)
-queryMETA(vcf,nice=FALSE)
-
-#Get file info:
-dat   <- gsub("#","",vcf@meta[grep("##file",vcf@meta)])
-nms   <- str_extract(dat,"^[^=]+")
-dat   <- set_names(str_extract(dat,"[^=]+$"),nms)
-
-#Fixed info:
-vcf@fix
-
-#Genotype info:
-vcf@gt
-
-
