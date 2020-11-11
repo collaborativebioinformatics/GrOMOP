00860939d954b57218a32e7a098f35bd9feb46f6
clugstonA <anc174@pitt.edu>
2020-11-11 11:54:54 -0500

    Replaced filepath references with here() anchors.

--- src/R/evs_romopomics_pipeline.R
+++ src/R/evs_romopomics_pipeline.R
@@ -1,20 +1,21 @@
-install.packages("pacman")
+#install.packages("pacman")
+#install_github("AndrewC160/ROMOPOmics",force=T)
 library(pacman)
-p_load(tidyverse,devtools,data.table,DBI)
-
-install_github("AndrewC160/ROMOPOmics",force=T)
 library(ROMOPOmics)
+library(here)
+
+p_load(tidyverse,devtools,data.table,DBI)
 
 #Data model.
 dm_file     <- system.file("extdata","OMOP_CDM_v6_0_custom.csv",package="ROMOPOmics",mustWork = TRUE)
 dm          <- loadDataModel(master_table_file = dm_file)
 
 #Mask file
-msk_file    <- file.path("../../data/evs_mask.tsv")
-msks <- loadModelMasks(msk_file)
+msk_file    <- here("data/evs_mask.tsv")
+msks        <- loadModelMasks(msk_file)
 
 #Sample file
-in_file     <- file.path("../../data/evs.tsv")
+in_file     <- here("data/evs.tsv")
 
 #Put it all together
 omop_inputs <- readInputFile(input_file=in_file,data_model=dm,mask_table=msks,transpose_input_table = T)
@@ -23,6 +24,6 @@ omop_inputs <- readInputFile(input_file=in_file,data_model=dm,mask_table=msks,tr
 db_inputs   <- combineInputTables(input_table_list = omop_inputs)
 
 #Bippity boppity boop
-omop_db     <- buildSQLDBR(omop_tables = db_inputs, sql_db_file=file.path("../../data/evs.sqlite"))
+omop_db     <- buildSQLDBR(omop_tables = db_inputs, sql_db_file=here("data/evs.sqlite"))
 DBI::dbDisconnect(omop_db)
 
--- src/R/sample_romopomics_pipeline.R
+++ src/R/sample_romopomics_pipeline.R
@@ -1,20 +1,20 @@
-install.packages("pacman")
+#install.packages("pacman")
+#install_github("AndrewC160/ROMOPOmics",force=T)
 library(pacman)
-p_load(tidyverse,devtools,data.table,DBI)
-
-install_github("AndrewC160/ROMOPOmics",force=T)
+library(here)
 library(ROMOPOmics)
+p_load(tidyverse,devtools,data.table,DBI)
 
 #Data model.
 dm_file     <- system.file("extdata","OMOP_CDM_v6_0_custom.csv",package="ROMOPOmics",mustWork = TRUE)
 dm          <- loadDataModel(master_table_file = dm_file)
 
 #Mask file
-msk_file    <- file.path("../../data/sample_mask.tsv")
-msks <- loadModelMasks(msk_file)
+msk_file    <- here("data/sample_mask.tsv")
+msks        <- loadModelMasks(msk_file)
 
 #Sample file
-in_file     <- file.path("../../data/sample.tsv")
+in_file     <- here("data/sample.tsv")
 
 #Put it all together
 omop_inputs <- readInputFile(input_file=in_file,data_model=dm,mask_table=msks,transpose_input_table = T)
@@ -23,6 +23,6 @@ omop_inputs <- readInputFile(input_file=in_file,data_model=dm,mask_table=msks,tr
 db_inputs   <- combineInputTables(input_table_list = omop_inputs)
 
 #Bippity boppity boop
-omop_db     <- buildSQLDBR(omop_tables = db_inputs, sql_db_file=file.path("../../data/sample.sqlite"))
+omop_db     <- buildSQLDBR(omop_tables = db_inputs, sql_db_file=here("data/sample.sqlite"))
 DBI::dbDisconnect(omop_db)
 
