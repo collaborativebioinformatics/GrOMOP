bf76fbbe9504f57ec2198bd5bfa21fbcc8ebf693
clugstonA <anc174@pitt.edu>
2020-11-11 19:00:54 -0500

    Merged toy dataset scripts to main branch.

---
+++ src/R/toy_data_database.R
@@ -0,0 +1,32 @@
+#!/bin/Rscript
+#Toy dataset database.
+library(ROMOPOmics)
+library(tidyverse)
+library(data.table)
+library(magrittr)
+library(here)
+
+rename    <- dplyr::rename
+mutate    <- dplyr::mutate
+
+dir_data  <- here("data/toy_data")
+
+#Option 1: Scan director(ies) for ESV files, count their flags.
+##Issue: where's the metadata going to come from?
+#Collect all TSV files and scan for flags.
+tb  <- tibble(evs_file=Sys.glob(file.path(dir_data,"*.tsv")),
+              met=scan_evs_flags(evs_file,flag_val="meta_driver"),
+              ind=scan_evs_flags(evs_file,flag_val="indel"),
+              coo=scan_evs_flags(evs_file,flag_val="cooccurring"))
+
+#Option 2: Load a table of data that includes files and metadata.
+# can still can for flags.
+##Issue: Where will this table come from?
+tb2 <- fread(Sys.glob(file.path(dir_data,"*.csv"))) %>%
+        mutate(met=scan_evs_flags(evs_file,flag_val="meta_driver"),
+               ind=scan_evs_flags(evs_file,flag_val="indel"),
+               coo=scan_evs_flags(evs_file,flag_val="cooccurring"))
+
+dim(tb)
+dim(tb2)
+
