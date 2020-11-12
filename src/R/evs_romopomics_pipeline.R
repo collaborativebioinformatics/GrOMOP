e61871916bff06d572833f4cc5764fbcb093344c
Nick Giangreco <npg2108@cumc.columbia.edu>
2020-11-11 22:38:48 -0500

    updated evs data with evs table correspondence in omop

--- src/R/evs_romopomics_pipeline.R
+++ src/R/evs_romopomics_pipeline.R
@@ -1,10 +1,9 @@
 #install.packages("pacman")
-#install_github("AndrewC160/ROMOPOmics",force=T)
 library(pacman)
-library(ROMOPOmics)
-library(here)
+p_load(tidyverse,devtools,data.table,DBI,here)
 
-p_load(tidyverse,devtools,data.table,DBI)
+#install_github("AndrewC160/ROMOPOmics",force=T)
+library(ROMOPOmics)
 
 #Data model.
 dm_file     <- system.file("extdata","OMOP_CDM_v6_0_custom.csv",package="ROMOPOmics",mustWork = TRUE)
